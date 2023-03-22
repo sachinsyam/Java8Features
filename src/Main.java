514f3d269ccc1a198998ded403683901ad73a9ab
Sachin <sachinsyam@gmail.com>
2023-03-22 12:38:02 +0530

    consumer interface

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import functionalInterface.ConsumerInterfaceExample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsumerInterfaceExample consumerInterfaceExample = new ConsumerInterfaceExample();
+        consumerInterfaceExample.main();
+
+
+    }
+
+
+}
---
+++ src/functionalInterface/ConsumerInterfaceExample.java
@@ -0,0 +1,34 @@
+package functionalInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerInterfaceExample {
+    //A consumer interface has 2 built in methods, 1. accept    2. andThen
+
+    public void main(){
+        System.out.println("Consumer Interface Example");
+        Consumer<Integer> print = a -> System.out.println(a);
+   //     print.accept(5);
+
+        Consumer<List<Integer>> printList = list -> list.stream().forEach(a -> System.out.println(a));
+
+        List<Integer> numList = new ArrayList<>();
+        numList.add(1);
+        numList.add(2);
+
+    //    printList.accept(numList);
+
+        Consumer<List<Integer>> modifyList = list -> {
+            for (Integer listItem: list
+                 ) {
+                list.set(list.indexOf(listItem),2*listItem);
+            }
+        };
+
+        //using andThen function of consumer
+        modifyList.andThen(printList).accept(numList);
+    }
+
+}
